ed91b07142b3a6ba98dc53fe8addcd03cb948936
AlexPRIvan <70669222+AlexPRIvan@users.noreply.github.com>
2024-05-02 18:14:29 +0000

    Algoritmo completado, funciona bíen

--- torre_de_hanoi.py
+++ torre_de_hanoi.py
@@ -1,9 +1,9 @@
 def torre_hanoi(n, origen, destino, auxiliar):
     if n==1:
         print("mover disco ", n, " de ", origen, " a ", destino)
-    return
+        return
     torre_hanoi(n-1, origen, auxiliar, destino)
     print("mover disco ", n, " de ", origen, " a ", destino)
     torre_hanoi(n-1, auxiliar, destino, origen) 
-n = 3
-torre_hanoi(n, 'A', 'B', 'C')
+n = 4
+torre_hanoi(n, 'A', 'C', "B")
