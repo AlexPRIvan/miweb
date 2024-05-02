23c7072441f426b66b3ec593c63a3eaaccddd57f
AlexPRIvan <70669222+AlexPRIvan@users.noreply.github.com>
2024-05-02 18:10:49 +0000

    Cambio del algoritmo

--- torre_de_hanoi.py
+++ torre_de_hanoi.py
@@ -1,15 +1,9 @@
 def torre_hanoi(n, origen, destino, auxiliar):
     if n==1:
-        print ("Mover disco 1 de torre ", origen, " a torre ", destino)
-        return 
+        print("mover disco ", n, " de ", origen, " a ", destino)
+    return
     torre_hanoi(n-1, origen, auxiliar, destino)
-    print("Mover disco ", n, " a torre ", origen, " a torre ", destino)
-    torre_hanoi(n-1, auxiliar, destino, origen)
-
-n=3
-
+    print("mover disco ", n, " de ", origen, " a ", destino)
+    torre_hanoi(n-1, auxiliar, destino, origen) 
+n = 3
 torre_hanoi(n, 'A', 'B', 'C')
-
-
-
-
